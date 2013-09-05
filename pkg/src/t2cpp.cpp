d62a3e014796be28a4b3c29ece5c5c3e2795eab7
jmsigner (johannes signer) <j.m.signer@gmail.com>
2013-09-05 19:53:32 +0000

    updates
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rhr@61 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/src/t2cpp.cpp
+++ pkg/src/t2cpp.cpp
@@ -1,6 +1,35 @@
 #include <Rcpp.h>
 #include <cmath>
 
+RcppExport SEXP t2cpp2(SEXP xx, SEXP yy, SEXP tt, SEXP interval) {
+  BEGIN_RCPP
+    //SEXP t2cpp(NumericVector x, NumericVector y, NumericVector t, int interval, int keep, int type) {
+    //using namespace Rcpp;
+  Rcpp::NumericVector x(xx);
+  Rcpp::NumericVector y(yy);
+  Rcpp::NumericVector t(tt);
+  Rcpp::NumericVector in(interval);
+    
+  double sumx = 0;
+  double sumy = 0;
+  int n = x.size();
+  Rcpp::NumericVector included(n);
+  included[0] = 1;
+
+  int tdiff = 0;
+  for (int i = 0; i < n; i++) {
+      tdiff += t[i] - t[i-1];
+      if (tdiff >= in[0]) {
+	included[i] = 1;
+	tdiff = 0;
+      } else  {
+	included[i] = 0;
+      }
+  }
+  return included;
+  END_RCPP
+}
+
 RcppExport SEXP t2cpp(SEXP xx, SEXP yy, SEXP tt, SEXP params) {
 BEGIN_RCPP
   //SEXP t2cpp(NumericVector x, NumericVector y, NumericVector t, int interval, int keep, int type) {
@@ -21,7 +50,7 @@ BEGIN_RCPP
   int tlower = interval - keep;
   int tupper = interval + keep;
   // type 1 - use all points that are reachable, regardless of the order; 2 - strict, only consecutive steps that macht the time interval; 3 - more flexible version of consecutive version, i.e. if no points fall within the time interval, take next possible
-  if (type == 1) {
+  if (type == 21) {
     for (int i = 1; i < n; i++) {
       for (int j = 0; j < (n-1); j++) {
 	int tdiff = t[i] - t[j];
@@ -34,7 +63,7 @@ BEGIN_RCPP
 	}
       }
     }
-  } else if (type == 2) {
+  } else if (type == 1) {
     int i = 0;
     while (i < n) {
       for (int j = i; j < n; j++) {
@@ -54,7 +83,7 @@ BEGIN_RCPP
       }
       i++;
     }
-  } else if (type == 3) {
+  } else if (type == 2) {
     int tdiff = 0;
     int cumdiff = 0;
     for (int i = 1; i < n; i++) {
@@ -71,5 +100,58 @@ BEGIN_RCPP
     }
   }
   return Rcpp::NumericVector::create(1/(double)m * (sumx + sumy), n, m);
+END_RCPP
+}
+
+RcppExport SEXP t2sub(SEXP xx, SEXP yy, SEXP tt, SEXP params) {
+BEGIN_RCPP
+  //SEXP t2cpp(NumericVector x, NumericVector y, NumericVector t, int interval, int keep, int type) {
+  //using namespace Rcpp;
+  Rcpp::NumericVector x(xx);
+  Rcpp::NumericVector y(yy);
+  Rcpp::NumericVector t(tt);
+  Rcpp::List rparam(params);
+
+  int interval = Rcpp::as<int>(rparam["interval"]);
+  int type = Rcpp::as<int>(rparam["type"]);
+  int keep =  Rcpp::as<int>(rparam["keep"]);
+    
+  int n = x.size();
+  Rcpp::NumericVector included(n);
+  int tlower = interval - keep;
+  int tupper = interval + keep;
+  // type 1 - strict, only consecutive steps that macht the time interval; 2 - more flexible version of consecutive version, i.e. if no points fall within the time interval, take next possible
+  if (type == 1) {
+    int i = 0;
+    while (i < n) {
+      for (int j = i; j < n; j++) {
+	int tdiff = t[j] - t[i];
+	if (tdiff >= tlower && tdiff <= tupper) {
+	  included[i] = 1;
+	  i = j;
+	  break;
+	} else if ( tdiff > tupper) {
+	  included[i] = 0;
+	  i = j;
+	  break;
+	}
+      }
+      i++;
+    }
+  } else if (type == 2) {
+    int tdiff = 0;
+    int cumdiff = 0;
+    for (int i = 1; i < n; i++) {
+      tdiff = t[i] - t[i - 1];
+      cumdiff += tdiff;
+      if (cumdiff >= interval) {
+	included[i] = 1;
+	cumdiff = 0;
+      } else {
+	included[i] = 0;
+      }
+    }
+  }
+  return included;
 END_RCPP
 }
--- pkg/src/t2cpp.h
+++ pkg/src/t2cpp.h
@@ -3,6 +3,8 @@
 
 #include <Rcpp.h>
 
+RcppExport SEXP t2cpp2(SEXP xx, SEXP yy, SEXP tt, SEXP interval);
 RcppExport SEXP t2cpp(SEXP x, SEXP y, SEXP t, int interval, int keep, int type);
+RcppExport SEXP t2sub(SEXP x, SEXP y, SEXP t, int interval, int keep, int type);
 
 #endif
